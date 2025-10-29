d176d95d37f808372a32ceaaa275cf4e467b0672
MahmoudGado1 <120124484+MahmoudGado1@users.noreply.github.com>
2025-10-29 19:15:03 +0300

    Remove trailing slash from baseURL in axiosInstance

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://mern-lms-server.vercel.app/",
+  baseURL: "https://mern-lms-server.vercel.app",
 });
 
 axiosInstance.interceptors.request.use(
